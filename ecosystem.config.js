1f189f4cfbc2cb54057cf52fc5f1306466aa9314
nikitashpota@ya.ru <nikitashpota@ya.ru>
2023-06-12 21:34:34 +0300

    add

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,11 @@
-module.exports = {
-  apps : [{
-    name: 'my-app',
-    script: 'app.js',
-    instances: 'max',
-    autorestart: true,
-    watch: true,
-    max_memory_restart: '1G',
-    env: {
-      NODE_ENV: 'production'
-    }
-  }]
-};
+export const apps = [{
+  name: 'my-app',
+  script: 'app.js',
+  instances: 'max',
+  autorestart: true,
+  watch: true,
+  max_memory_restart: '1G',
+  env: {
+    NODE_ENV: 'production'
+  }
+}];
